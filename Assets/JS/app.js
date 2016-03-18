4a44334d35267b3172dcbbadc24285264b870419
Dennis Goldin <dennis.goldin@gmail.com>
2016-03-18 06:52:47 -0400

    Add angular routing and empty controllers

---
+++ Assets/JS/app.js
@@ -0,0 +1,34 @@
+// Define an angular module for our app
+var storeApp = angular.module('storeApp', ['ngRoute']);
+
+// Routing
+// ProductView -> template product-view.html and Controller ProductViewController
+// Details -> template details.html and Controller DetailsViewController
+// Cart -> template details.html and Controller CartViewController
+// Receipt -> template details.html and Controller ReceiptViewController
+storeApp.config(['$routeProvider', function($routeProvider) {
+  $routeProvider
+    // .when('/', {
+    //   templateUrl: 'index.html',
+    //   controller: 'ProductViewController'
+    // })
+    .when('/ProductView', {
+      templateUrl: 'Partials/product-view.html',
+      controller: 'ProductViewController'
+    })
+    .when('/Details', {
+      templateUrl: 'Partials/details.html',
+      controller: 'DetailsViewController'
+    })
+    .when('/Cart', {
+       templateUrl: 'Partials/cart.html',
+        controller: 'CartViewController'
+    })
+    .when('/Receipt', {
+      templateUrl: 'Partials/receipt.html',
+      controller: 'ReceiptViewController'
+    })
+    .otherwise({
+      redirectTo: '/ProductView'
+    });
+}]);
---
+++ Assets/JS/controllers.js
@@ -0,0 +1,24 @@
+// Controllers
+storeApp.controller('ProductViewController', function($scope) {
+
+	$scope.message = 'This is the product view content';
+
+});
+
+storeApp.controller('DetailsViewController', function($scope) {
+
+	$scope.message = 'This is the details content';
+
+});
+
+storeApp.controller('CartViewController', function($scope) {
+
+	$scope.message = 'This is the cart content';
+
+});
+
+storeApp.controller('ReceiptViewController', function($scope) {
+
+	$scope.message = 'This is the receipt content';
+
+});
