4159b0e418f4145c2a2ce66077fb627f5ef7ce42
Dennis Goldin <dennis.goldin@gmail.com>
2016-03-19 11:40:04 -0400

    Add latest bug fix iteration - still has some major bugs

--- Assets/JS/controllers.js
+++ Assets/JS/controllers.js
@@ -2,41 +2,97 @@
 storeApp.controller('ProductViewController', ['$http', '$scope', function($http, $scope) {
 
 	// $http.get('http://localhost:3000/products')
-		$http.get('Assets/iron-store.json')
-					.success(function (data) {
-						console.log("Success!", data)
-					})
-         .error(function (data) {
-            console.log('There was an error!', data);
-
-					$scope.message = 	data.products;
-
-  });
+	$http.get('Assets/iron-store.json')
+		.success(function (data) {
+			console.log("Success!", data);
+			$scope.products = data;
+		})
+   .error(function (data) {
+      console.log('There was an error!', data);
+		});
 
 	$scope.message = 'This is the product view content';
 
-	// $scope.message = 	products;
+	// $scope.message = products;
 
-	// $scope.products = [
-  //   { name: 'Blue Iron',
-	// 	  price: '$20.00',
-	// 		description: 'The sweetest blue iron eva',
-	// 		image: 'http://some-image-file.png',
-	// 		reviews: ["my Review #1", "my Review #2", "my Review #3"]
-	// 	},
-  //   { name: 'Yellow Iron',
-	// 	  price: '$27.00',
-	// 		description: 'The sweetest yellow iron eva',
-	// 		image: 'http://some-image-file.png',
-	// 		reviews: ["my Review #1", "my Review #2", "my Review #3"],
-	// 	},
-	// 	{ name: 'Red Iron',
-	// 	  price: '$18.00',
-	// 		description: 'The sweetest red iron eva',
-	// 		image: 'http://some-image-file.png',
-	// 		reviews: ["my Review #1", "my Review #2", "my Review #3"]
-	// 	}
-  // ];
+	// $scope.products =
+	// {
+	// 	'products': [
+	//     {
+	// 			'name' : 'Blue Iron',
+	// 		  'price' : '20',
+	// 			'description' : 'The sweetest blue iron eva',
+	// 			'image' : 'http://some-image-file.png',
+	// 			'reviews' :
+	// 				[
+	// 					{
+	// 						'author' : "Some Other Author",
+	// 						'review' : "my Review #1",
+	// 					 	'stars' :  "1"
+	// 					},
+	// 					{
+	// 						'author' : "Some Other Author",
+	// 						'review' : "my Review #2",
+	// 					 	'stars' :  "4"
+	// 					},
+	// 					{
+	// 						'author' : "Another Author",
+	// 						'review' : "my Review #3",
+	// 						'stars' :	"3"
+	// 					}
+	// 				]
+	// 		},
+	//     {
+	// 			'name' : 'Yellow Iron',
+	// 		  'price' : '27.9',
+	// 			'description' : 'The sweetest yellow iron eva',
+	// 			'image' : 'http://some-image-file.png',
+	// 			'reviews' :
+	// 				[
+	// 					{
+	// 						'author' : "Some Author",
+	// 						'review' : "my Review #1",
+	// 					 	'stars' :  "1"
+	// 					},
+	// 					{
+	// 						'author' : "Some Other Author",
+	// 						'review' : "my Review #2",
+	// 					 	'stars' :  "4"
+	// 					},
+	// 					{
+	// 						'author' : "Another Author",
+	// 						'review' : "my Review #3",
+	// 						'stars' :	"3"
+	// 					}
+	// 				]
+	// 		},
+	// 		{
+	// 			'name' : 'Red Iron',
+	// 		  'price' : '18.99',
+	// 			'description' : 'The sweetest red iron eva',
+	// 			'image' : 'http://some-image-file.png',
+	// 			'reviews' :
+	// 				[
+	// 					{
+	// 						'author' : "Some Author",
+	// 						'review' : "my Review #1",
+	// 					 	'stars' :  "1"
+	// 					},
+	// 					{
+	// 						'author' : "Some Author",
+	// 						'review' : "my Review #2",
+	// 					 	'stars' :  "4"
+	// 					},
+	// 					{
+	// 						'author' : "Another Author",
+	// 						'review' : "my Review #3",
+	// 						'stars' :	"3"
+	// 					}
+	// 				]
+	// 			}
+	//   ]
+	// }
+	// console.log($scope.products);
 
 }]);
 
